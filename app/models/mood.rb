2a5bada1ef9075bcd75efd4cc256d36c903c0d5a
violaine soulas <vhsoulas@gmail.com>
2025-03-15 12:38:17 +0100

    Begin Calendar

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,3 +1,25 @@
 class Mood < ApplicationRecord
   belongs_to :user
+
+  validates :value, presence: true, inclusion: { in: 1..6 }
+  validates :date, presence: true
+
+
+  def color
+    case value
+    when 1 then 'red'
+    when 2 then 'orange'
+    when 3 then 'yellow'
+    when 4 then 'green'
+    when 5 then 'blue'
+    when 6 then 'purple'
+    else 'gray' # Valeur par défaut
+    end
+  end
+
+    # Scope pour récupérer la dernière humeur par jour, utilisant DISTINCT ON
+  scope :latest_per_day, -> {
+    select("DISTINCT ON (DATE(date)) *")  # Récupérer la première ligne pour chaque jour
+      .order("DATE(date) DESC, created_at DESC")  # Trier par date et ensuite par la date de création pour obtenir la plus récente
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_14_191806) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_15_110046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_14_191806) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["user_id"], name: "index_moods_on_user_id"
   end
 
