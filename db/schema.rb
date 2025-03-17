6fdaf68ab68519cae75d2ac0379b45ba08af475a
violaine soulas <vhsoulas@gmail.com>
2025-03-17 14:46:58 +0100

    Traduction pour devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_16_140839) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_131537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
