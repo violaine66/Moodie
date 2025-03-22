e5ad73bb9b59e5fcd6714e03a157f9a04487cd1b
Miaro Raohilison <miaro98@gmail.com>
2025-03-22 17:28:44 +0100

    Ressource et pres de chez moi

--- app/controllers/ressources_controller.rb
+++ app/controllers/ressources_controller.rb
@@ -1,9 +1,16 @@
 class RessourcesController < ApplicationController
+  before_action :set_ressources, only: %i[index pres_de_chez_moi]
+
   def index
-    @ressources = Ressource.all
   end
 
   def pres_de_chez_moi
-    @ressources.where(category: "Pres de chez moi")
+    @pres_de_chez_moi = @ressources.where(category: "Pres de chez moi")
+  end
+
+  private
+
+  def set_ressources
+    @ressources = Ressource.all
   end
 end
