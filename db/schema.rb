6ad5f15833e4b4db384b9b63e0d49b42699902e1
violaine soulas <vhsoulas@gmail.com>
2025-03-23 14:24:10 +0100

    Ajout cloudinary + téléchargement emotji sur cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_20_120842) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_23_130328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "chat_messages", force: :cascade do |t|
     t.string "user_message"
@@ -26,7 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_120842) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "date", default: -> { "CURRENT_DATE" }
+    t.date "date"
     t.index ["user_id"], name: "index_journal_posts_on_user_id"
   end
 
@@ -36,6 +64,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_120842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date"
+    t.string "emotion"
+    t.text "emotions"
     t.index ["user_id"], name: "index_moods_on_user_id"
   end
 
@@ -66,6 +96,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_120842) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "journal_posts", "users"
   add_foreign_key "moods", "users"
 end
